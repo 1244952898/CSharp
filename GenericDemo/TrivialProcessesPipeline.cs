99ee24f7961612856710088b577d9650935a19c4
zhuangyu@yiche.com <zhuangyu@yiche.com>
2018-12-11 20:13:36 +0800

    shipper

---
+++ GenericDemo/TrivialProcessesPipeline.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    public class TrivialProcessesPipeline: Pipeline<Process>
+    {
+        public TrivialProcessesPipeline()
+        {
+            Register(new GetAllProcesses());
+            Register(new LimitByWorkingSetSize());
+            Register(new PrintProcessName());
+        }
+    }
+}
