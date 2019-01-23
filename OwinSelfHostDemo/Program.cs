b61391cb939fb34242d93624dd2debc10200ba58
zhuangyu@yiche.com <zhuangyu@yiche.com>
2019-01-23 16:09:20 +0800

    1

--- OwinSelfHostDemo/Program.cs
+++ OwinSelfHostDemo/Program.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using OwinSelfHostDemo.OWIN;
+using OwinSelfHostDemo.TopSelfclasses;
+using OwinSelfHostDemo.TopShelfClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.HostConfigurators;
 
 namespace OwinSelfHostDemo
 {
@@ -10,6 +17,16 @@ namespace OwinSelfHostDemo
     {
         static void Main(string[] args)
         {
+            //TopShelfConfigure.Config1();
+            string baseAddress = "http://localhost:9000/";
+            using (WebApp.Start<Startup>(baseAddress))
+            {
+                HttpClient httpClient = new HttpClient();
+                var response = httpClient.GetAsync(baseAddress + "api/values").Result;
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ OwinSelfHostDemo/TopShelfClasses/MyService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+using Topshelf.Runtime;
+
+namespace OwinSelfHostDemo.TopShelfClasses
+{
+    public class MyService : ServiceControl
+    {
+        public MyService()
+        {
+        }
+        public MyService(HostSettings settings)
+        {
+        }
+
+        public bool Start(HostControl hostControl)
+        {
+            Console.WriteLine("Start");
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MyStart() {
+            Console.WriteLine("MyStart");
+        }
+
+        public void MyStop()
+        {
+            Console.WriteLine("MyStop");
+        }
+
+        public void WhenPaused()
+        {
+            Console.WriteLine("WhenPaused");
+        }
+
+        public void WhenContinued()
+        {
+            Console.WriteLine("WhenContinued");
+        }
+
+        public void WhenShutdown()
+        {
+            Console.WriteLine("WhenShutdown");
+        }
+    }
+}
