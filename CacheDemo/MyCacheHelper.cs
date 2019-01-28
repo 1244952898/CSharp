6bdc76777ce5d977aa8d2863cb0e9a2d5ef8db35
zhuangyu@yiche.com <zhuangyu@yiche.com>
2019-01-28 11:02:52 +0800

    1

---
+++ CacheDemo/MyCacheHelper.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheDemo
+{
+    public class MyCacheHelper : ICache
+    {
+        private static Dictionary<string, KeyValuePair<object,DateTime>> _Dictionary = new Dictionary<string, KeyValuePair<object, DateTime>>();
+
+        public object this[string key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public int Count => throw new NotImplementedException();
+
+        public void Add(string key, object data, int cacheTime = 30)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return;
+            }
+            KeyValuePair<object, DateTime> keyValuePair = new KeyValuePair<object, DateTime>(data, DateTime.Now.AddMinutes(cacheTime));
+
+            _Dictionary.Add(key, keyValuePair);
+
+        }
+
+        public bool Contains(string key)
+        {
+            if (_Dictionary.ContainsKey(key))
+            {
+                KeyValuePair<object, DateTime> keyValuePair = _Dictionary[key];
+                if (keyValuePair.Value >= DateTime.Now) {
+                    return true;
+                }
+                else
+                {
+                    _Dictionary.Remove(key);
+                }
+            }
+            return false;
+        }
+
+        public T Get<T>(string key)
+        {
+            if (_Dictionary.ContainsKey(key))
+            {
+                KeyValuePair<object, DateTime> keyValuePair = _Dictionary[key];
+                if (keyValuePair.Value<DateTime.Now)
+                {
+                    _Dictionary.Remove(key);
+                    return default(T);
+                }
+                return (T)keyValuePair.Key;
+            }
+            else
+                return default(T);
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CacheDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("欢迎来到.net高级班vip课程，今天是Eleven老师为大家带来的缓存的学习");
+                for (int i = 0; i < 5; i++)
+                {
+                    //List<Program> programs = DBHelper.Query<Program>();
+                }
+                for (int i = 0; i < 5; i++)
+                {
+                    if (CacheManager.Contains("RemoteHelper"))
+                    {
+                        List<Program> programs = CacheManager.GetData<List<Program>>("RemoteHelper");
+                    }
+                    else
+                    {
+                        List<Program> programs = RemoteHelper.Query<Program>();
+                        CacheManager.Add("RemoteHelper", programs);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
