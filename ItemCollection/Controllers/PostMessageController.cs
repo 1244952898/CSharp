bb0ea639d7a31dffec93f5625a5eb2a038ee26ab
zhuangyu@yiche.com <zhuangyu@yiche.com>
2019-03-27 09:33:44 +0800

    34

---
+++ ItemCollection/Controllers/PostMessageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ItemCollection.Controllers
+{
+    public class PostMessageController : Controller
+    {
+       /// <summary>
+       /// 
+       /// </summary>
+       /// <returns></returns>
+        public ActionResult PostMessageA()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult PostMessageB()
+        {
+            return View();
+        }
+    }
+}
