387330860700a8107b97ba3e8fefffbf81a60265
zhuangyu@yiche.com <zhuangyu@yiche.com>
2019-04-09 18:14:52 +0800

    1

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharp.字符串回文;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,28 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
+
+            string s = "123qwertyuuytrewq321";
+            string a = s.Substring(0, 3);
+            Console.WriteLine(a);
+            bool result = StringCycle.IsCycleMethod(s);
+            Console.WriteLine("{0}是否是：{1}", s, result);
+
+            string s1 = "fedca123qwertyuuytrewq321abcdef__123321++qwertyytrewq";
+            string result1 = StringCycle.GetCycleString1(s1);
+            Console.WriteLine("result1最长是：{0}", result1);
+
+            string result2 = StringCycle.GetCycleString2(s1);
+            Console.WriteLine("result2最长是：{0}", result2);
+
+            string result3 = StringCycle.GetCycleString3("fedca123qwertyu1uytrewq321abcdef__123321++qwertyytrewq");
+            string result4 = StringCycle.GetCycleString3(s1);
+            Console.WriteLine("result3最长是：{0}", result3);
+            Console.WriteLine("result4最长是：{0}", result4);
+
             Parent p=new Parent();
             int? d = p.Childrens?.Count;
+            Console.ReadKey();
         }
     }
 }
