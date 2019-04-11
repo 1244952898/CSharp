c1dbcadb69c51fab3ec9ffaab20e388ce688aeff
zhuangyu@yiche.com <zhuangyu@yiche.com>
2019-04-11 15:58:24 +0800

    1

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,34 +4,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CSharp.Queens;
 namespace CSharp
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Queen.FindQueen(0);
 
-            string s = "123qwertyuuytrewq321";
-            string a = s.Substring(0, 3);
-            Console.WriteLine(a);
-            bool result = StringCycle.IsCycleMethod(s);
-            Console.WriteLine("{0}是否是：{1}", s, result);
 
-            string s1 = "fedca123qwertyuuytrewq321abcdef__123321++qwertyytrewq";
-            string result1 = StringCycle.GetCycleString1(s1);
-            Console.WriteLine("result1最长是：{0}", result1);
+            //string s = "123qwertyuuytrewq321";
+            //string a = s.Substring(0, 3);
+            //Console.WriteLine(a);
+            //bool result = StringCycle.IsCycleMethod(s);
+            //Console.WriteLine("{0}是否是：{1}", s, result);
 
-            string result2 = StringCycle.GetCycleString2(s1);
-            Console.WriteLine("result2最长是：{0}", result2);
+            //string s1 = "fedca123qwertyuuytrewq321abcdef__123321++qwertyytrewq";
+            //string result1 = StringCycle.GetCycleString1(s1);
+            //Console.WriteLine("result1最长是：{0}", result1);
 
-            string result3 = StringCycle.GetCycleString3("fedca123qwertyu1uytrewq321abcdef__123321++qwertyytrewq");
-            string result4 = StringCycle.GetCycleString3(s1);
-            Console.WriteLine("result3最长是：{0}", result3);
-            Console.WriteLine("result4最长是：{0}", result4);
-            //#[^#]+#
-            Parent p =new Parent();
-            int? d = p.Childrens?.Count;
+            //string result2 = StringCycle.GetCycleString2(s1);
+            //Console.WriteLine("result2最长是：{0}", result2);
+
+            //string result3 = StringCycle.GetCycleString3("fedca123qwertyu1uytrewq321abcdef__123321++qwertyytrewq");
+            //string result4 = StringCycle.GetCycleString3(s1);
+            //Console.WriteLine("result3最长是：{0}", result3);
+            //Console.WriteLine("result4最长是：{0}", result4);
+            ////#[^#]+#
+            //Parent p =new Parent();
+            //int? d = p.Childrens?.Count;
             Console.ReadKey();
         }
     }
