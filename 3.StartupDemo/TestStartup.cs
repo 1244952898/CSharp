0186d48dbf9ad23f5f1941723666344d458eae88
zhuangyu@yiche.com <zhuangyu@yiche.com>
2019-05-20 14:36:32 +0800

    1

---
+++ 3.StartupDemo/TestStartup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup("TestingConfiguration", typeof(_3.StartupDemo.TestStartup))]
+
+namespace _3.StartupDemo
+{
+   
+    public class TestStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // 有关如何配置应用程序的详细信息，请访问 https://go.microsoft.com/fwlink/?LinkID=316888
+            app.Run(context =>
+            {
+                string t = DateTime.Now.Millisecond.ToString();
+                return context.Response.WriteAsync(t + " Test OWIN App");
+            });
+        }
+    }
+}
