a6668eb9eb7a927b72758f6d4348f3fc46cb645f
zhuangyu <zhuangyu@yiche.com>
2019-08-08 14:26:32 +0800

    1

---
+++ VueDemo/Controllers/VueController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VueDemo.Controllers
+{
+    public class VueController : Controller
+    {
+        // GET: vue0
+        public ActionResult v0()
+        {
+            return View();
+        }
+    }
+}
