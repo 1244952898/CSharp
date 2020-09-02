d7c059e305cc74b40924cf674a38bb635d8df0fd
zhuangyu <zhuangyu@yiche.com>
2020-09-02 13:50:13 +0800

    1

--- ES0/Program.cs
+++ ES0/Program.cs
@@ -25,29 +25,53 @@ namespace ES0
             var settings1 = new ConnectionConfiguration(connectPool);
             var lowlevelClient1 = new ElasticLowLevelClient(settings1);
 
-            var person = new Person
-            {
-                FirstName = "Martijn",
-                LastName = "Laarman"
-            };
-            var indexRespose = lowlevelClient.Index<BytesResponse>("people", "1", PostData.Serializable(person));
-            byte[] responseBytes = indexRespose.Body;
+            //var person = new Person
+            //{
+            //    FirstName = "Martijn",
+            //    LastName = "Laarman"
+            //};
+            //var indexRespose = lowlevelClient.Index<BytesResponse>("people", "1", PostData.Serializable(person));
+            //byte[] responseBytes = indexRespose.Body;
 
             //var asyncIndexResponse = await lowlevelClient.IndexAsync<StringResponse>("people", "1", PostData.Serializable(person));
             //string responseString = asyncIndexResponse.Body;
 
-            var peoples = new object[]
-            {
-                new { index = new { _index = "people", _type = "person", _id = "1"  }},
-                new { FirstName = "Martijn", LastName = "Laarman" },
-                new { index = new { _index = "people", _type = "person", _id = "2"  }},
-                new { FirstName = "Greg", LastName = "Marzouka" },
-                new { index = new { _index = "people", _type = "person", _id = "3"  }},
-                new { FirstName = "Russ", LastName = "Cam" },
-            };
+            //var peoples = new object[]
+            //{
+            //    new { index = new { _index = "people", _type = "_doc", _id = "1"  }},
+            //    new { FirstName = "Martijn", LastName = "Laarman" },
+            //    new { index = new { _index = "people", _type = "_doc", _id = "2"  }},
+            //    new { FirstName = "Greg", LastName = "Marzouka" },
+            //    new { index = new { _index = "people", _type = "_doc", _id = "3"  }},
+            //    new { FirstName = "Russ", LastName = "Cam" },
+            //};
+            //var ndexResponse = lowlevelClient.Bulk<StringResponse>(PostData.MultiJson(peoples));
+            //string responseStream = ndexResponse.Body;
 
-            var ndexResponse = lowlevelClient.Bulk<StringResponse>(PostData.MultiJson(peoples));
-            string responseStream = ndexResponse.Body;
+            //var searchResponse = lowlevelClient.Search<StringResponse>("people", PostData.Serializable(new
+            //{
+            //    from = 0,
+            //    size = 10,
+            //    query = new
+            //    {
+            //        match = new
+            //        {
+            //            firstName = new
+            //            {
+            //                query = "Martijn"
+            //            }
+            //        }
+            //    }
+            //}));
+
+            //var successful = searchResponse.Success;
+            //var responseJson = searchResponse.Body;
+
+            //var searchResponse1 = lowlevelClient.Search<BytesResponse>("people", PostData.Serializable(new { match_all = new { } }));
+
+            //var success = searchResponse1.Success;
+            //var successOrKnownError = searchResponse1.SuccessOrKnownError;
+            //var exception = searchResponse1.OriginalException;
         }
     }
 }
