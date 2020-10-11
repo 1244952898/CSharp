46370c23eaca0fe6165d7efee97379e788be2604
23DesignModel <1244952898@qq.com>
2020-10-11 22:08:37 +0800

    哦

---
+++ Qiniu/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Qiniu.Storage;
+using Qiniu.Util;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Qiniu.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+           var AccessKey = ConfigurationManager.AppSettings["AccessKey"];
+           var SecretKey = ConfigurationManager.AppSettings["SecretKey"];
+            Mac mac = new Mac(AccessKey, SecretKey);
+
+            PutPolicy putPolicy = new PutPolicy();
+            putPolicy.Scope = "sensori-south-bucket.s3-cn-south-1.qiniucs.com";
+            string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+
+            return View();
+        }
+    }
+}
---
+++ Qiniu1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Qiniu.Storage;
+using Qiniu.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qiniu1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime st = DateTime.UtcNow;
+            //DateTime.Now.Kind == DateTimeKind.Unspecified;
+            DateTime time = DateTime.SpecifyKind(DateTime.Now, DateTimeKind.Local);
+
+
+            Mac mac = new Mac("Q55UE0jgwwHxF8eH3bXCYAbTo7Q7I_QhJAiXk4gB", "6MuXJVexC7qsY8Iu9TbWXP4097TEx0IRIT8_LHtz");
+            PutPolicy putPolicy = new PutPolicy();
+            putPolicy.Scope = "sensori-south-bucket.s3-cn-south-1.qiniucs.com";
+            string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
+        }
+    }
+}
