35221ac9611ef756f0ca56159a5358435de5c8f5
23DesignModel <1244952898@qq.com>
2020-11-01 11:06:33 +0800

    1

---
+++ DeleagetAndEvent/NewFolder1/GreetPeople.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DeleagetAndEvent.Program;
+
+namespace DeleagetAndEvent.NewFolder1
+{
+    public class GreetPeopleModel
+    {
+        /// <summary>
+        /// 在 GreetingManager 类的内部声明 delegate1 变量
+        /// </summary>
+        public event GreetingDelegate delegateEvent;
+        public void GreetPeople(string name)
+        {
+            delegateEvent(name);
+        }
+    }
+}
--- DeleagetAndEvent/Program.cs
+++ DeleagetAndEvent/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DeleagetAndEvent.NewFolder1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +7,55 @@ using System.Threading.Tasks;
 
 namespace DeleagetAndEvent
 {
+    public delegate void GreetingDelegate(string name);
     class Program
     {
 
         static void Main(string[] args)
         {
-            IPhone6 iphone6 = new IPhone6() { Price = 5288M };
-            // 订阅事件         
-            iphone6.priceChange += iphone6_PriceChanged;
-            // 调整价格（事件发生）        
-            iphone6.Price = 3999;
+            //Heater heater = new Heater();
+            //Alarm alarm = new Alarm();
+            //heater.Boiled += alarm.MakeAlert; //注册方法
+            //heater.Boiled += (new Alarm()).MakeAlert; //给匿名对象注册方法
+            //heater.Boiled += new Heater.BoilHandler(alarm.MakeAlert); //也可以这么注册
+            //heater.Boiled += Display.ShowMsg; //注册静态方法
+            //heater.BoilWater(); //烧水，会自动调用注册过对象的方法
+            //heater.BoilWater();
+            //var greetPeopleModel = new GreetPeopleModel();
+            //greetPeopleModel.delegateEvent = EGreet;
+            //greetPeopleModel.GreetPeople("abc");
+            //GreetingDelegate greetPeopleDelegate = EGreet;
+            //greetPeopleDelegate += CGreet;
+            //Greet("aaa", greetPeopleDelegate);
+            //Greet("bbb", greetPeopleDelegate);
 
-            IPhone11 phone11 = new IPhone11();
-            phone11.ClickEventHandler += Click11;
+            #region MyRegion
+            //IPhone6 iphone6 = new IPhone6() { Price = 5288M };
+            //// 订阅事件         
+            //iphone6.priceChange += iphone6_PriceChanged;
+            //// 调整价格（事件发生）        
+            //iphone6.Price = 3999;
+
+            //IPhone11 phone11 = new IPhone11();
+            //phone11.ClickEventHandler += Click11;
+            #endregion
+
 
             Console.ReadKey(); 
+        }
+
+        public static void EGreet(string nme)
+        {
+            Console.WriteLine($"{nme} EGreet");
+        }
+        public static void CGreet(string nme)
+        {
+            Console.WriteLine($"{nme} CGreet");
+        }
+
+        public static void Greet(string name , GreetingDelegate greetPeopleDelegate)
+        {
+            greetPeopleDelegate(name);
         }
 
         private static void Click11(object sender, ClickEventArgs e)
