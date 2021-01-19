6ca2c59d1f5b82fc6f9af3f66386985a46b4bec7
23DesignModel <1244952898@qq.com>
2021-01-19 08:59:52 +0800

    1

---
+++ 1.Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using _1.Array.methods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MyRegion
+            int[] nums = new int[] { 1, 1, 3, 4, 5, 5, 6 };
+            int num = RemoveDuplicates.RemoveDuplicates0(nums);
+            Console.WriteLine(num);
+            Console.WriteLine("数组");
+            for (int i = 0; i < num; i++)
+            {
+                Console.Write(nums[i]+" ");
+            }
+            #endregion
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1.Array/methods/删除排序数组中的重复项.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Array.methods
+{
+    public class RemoveDuplicates
+    {
+        public static int RemoveDuplicates0(int[] nums)
+        {
+            if (nums==null||nums.Length<=0)
+                return 0;
+            int i = 0;
+            int j = 1;
+            for (; j < nums.Length; j++)
+            {
+                if (nums[i]!=nums[j])
+                {
+                    i++;
+                    if (i!=j)
+                    {
+                        nums[i] = nums[j];
+                    }
+                }
+            }
+            return i+1;
+        }
+    }
+}
