fa6ee92a9ad17243ea13ec1180d9b295862e7bfa
Yu Zhuang <yu.zhuang@feisystems.com>
2022-11-03 05:16:15 -0400

    1

---
+++ DelegateAndEvent/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DelegateAndEvent
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Version2
+
+            {
+
+            }
+
+            #endregion
+
+        }
+    }
+}
