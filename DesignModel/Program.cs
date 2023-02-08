c5a8c906b6414c5398b3258383d92754d301228c
庄玉 <1244952898@qq.com>
2023-02-08 16:21:52 +0800

    1

--- DesignModel/Program.cs
+++ DesignModel/Program.cs
@@ -6,11 +6,25 @@ namespace DesignModel
 {
     internal class Program
     {
+
+        /*
+         单一职责：就一个类而言，应该仅有一个引起它变化点原因
+
+
+        2.策略模式：定义了算法家族，分别封装起来，让它们之间可以互相替换。此模式让算法的变化，不会影响到使用算法的客户。
+
+        3.装饰器模式：动态的给一个对象添加额外的职责，装饰器模式比子类更加灵活
+
+        4.代理模式：为其他对象提供一种代理以控制对这个对象的访问
+
+        5.用原型实例指定创建对象的种类，并通过拷贝这些原形创建新的对象
+
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            ClainOfResponsibilityDemo.main();
-            Console.ReadLine();
+            //Console.WriteLine("Hello World!");
+            //ClainOfResponsibilityDemo.main();
+            //Console.ReadLine();
 
             //ConcurrentDictionary<int,string> keyValuePairs= new ConcurrentDictionary<int,string>();
         }
