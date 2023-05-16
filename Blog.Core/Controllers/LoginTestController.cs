7b7470e66795351f9bc0135a2bb508f711d3a69b
庄玉 <1244952898@qq.com>
2023-05-16 20:51:11 +0800

    1

--- Blog.Core/Controllers/LoginTestController.cs
+++ Blog.Core/Controllers/LoginTestController.cs
@@ -29,28 +29,6 @@ namespace Blog.Core.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Login(string returnUrl = null)
-        {
-            const string Issuer = "https://gov.uk";
-            var claims = new List<Claim> {
-               new Claim(ClaimTypes.Name,"Andrew",ClaimValueTypes.String,Issuer),
-               new Claim(ClaimTypes.Surname,"Lock",ClaimValueTypes.String,Issuer),
-               new Claim(ClaimTypes.Country,"UK",ClaimValueTypes.String,Issuer),
-               new Claim(ClaimTypes.Surname,"Lock",ClaimValueTypes.String,Issuer),
-                new Claim("ChildhoodHero", "Ronnie James Dio", ClaimValueTypes.String)
-            };
-
-            var claimIdentity=new ClaimsIdentity(claims,"DriverLicense");
-            var claimPrincipal=new ClaimsPrincipal(claimIdentity);
-
-            await HttpContext.SignInAsync("Cookie", claimPrincipal, new AuthenticationProperties
-            {
-                ExpiresUtc = DateTime.UtcNow.AddMinutes(30),
-                IsPersistent = false,
-                AllowRefresh = false,
-            });
-
-            return RedirectToLocal(returnUrl);
-        }
+  
     }
 }
