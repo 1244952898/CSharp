e968bc7f693a4cf3665a3f0b898babeac95faede
zy <zy12449528981@outlook.com>
2023-06-28 16:30:26 +0800

    1

---
+++ CSharp/A/ArasInnovator.cs
@@ -0,0 +1,23 @@
+﻿using Aras.IOM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.A
+{
+    internal class ArasInnovator
+    {
+        public void M()
+        {
+            HttpServerConnection httpServerConnection = IomFactory.CreateHttpServerConnection("http://localhost/InnovatorServer/", "InnovatorSolutions_test", "admin", "innovator");
+            var item = httpServerConnection.Login();
+            if (item.isError())
+            {
+            }
+            Innovator inn = IomFactory.CreateInnovator(httpServerConnection);
+            Item item1=inn.newItem();
+        }
+    }
+}
