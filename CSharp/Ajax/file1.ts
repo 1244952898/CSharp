8bef4619b6edb9e9342d272609fa8cfc4d2199bb
zy <zy12449528981@outlook.com>
2023-07-11 16:26:45 +0800

    1

---
+++ CSharp/Ajax/file1.ts
@@ -0,0 +1,18 @@
+﻿function myAjax(url) {
+    let prm = new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState != 4) {
+                return;
+            }
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                resolve(xhr.responseText);
+            } else {
+                reject("服务器错误！");
+            }
+        }
+        xhr.open('get', url);
+        xhr.send(null);
+    });
+    return prm;
+}
