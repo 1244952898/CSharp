c94e328e663bd3071d7f10c809f0af5e5d8f2103
zy <zy12449528981@outlook.com>
2023-08-02 13:06:04 +0800

    1

---
+++ CSharp/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp
+{
+    internal class Class1
+    {
+        public string Name1 { get; set; }
+        public string Name2 { get; set; }
+        public string Name3 { get; set; }
+        public string Name4 { get; set; }
+        public string Name5 { get; set; }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
-
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharp
 {
@@ -7,10 +9,59 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            var discs=new Dictionary<string, string>();
-            var ids=new List<string>();
-            ids.Add("a");
+            List<Class1> cls1 = new List<Class1>();
+            cls1.Add(new Class1
+            {
+                Name1 = "1",
+                Name2 = "2",
+                Name3 = "3",
+                Name4 = "4",
+                Name5 = "5",
+            });
+            cls1.Add(new Class1
+            {
+                Name1 = "11",
+                Name2 = "22",
+                Name3 = "33",
+                Name4 = "44",
+                Name5 = "55",
+            });
+            cls1.Add(new Class1
+            {
+                Name1 = "111",
+                Name2 = "222",
+                Name3 = "333",
+                Name4 = "444",
+                Name5 = "5",
+            });
+            var cls2 = cls1.GroupBy(cls =>
+            {
+                return cls.Name5;
+            });
+            var ll = cls2.ToList();
+            
+            //var discs=new Dictionary<string, string>();
+            //var ids=new List<string>() { "a","b","c","d"};
+            ////var ids1=ids.GroupBy()
+            //var sdafd =ids.OrderBy(x=>x.Contains("a")).ToList();
+            //Console.WriteLine(sdafd.Count);
+            //var idsEx = ids.GetEnumerator();
+            //while (idsEx.MoveNext())
+            //{
+            //    //ids.Remove(id);
+            //}
+            //foreach (var id in ids)
+            //{
+            //    ids.Remove(id);
+            //    Console.WriteLine(id);
+            //}
+            //var ids1=new List<string>();
+            //ids.Add("a");
+            //ids1.Add("b");
 
+            //ids.Union(ids1);
+            //ids.Except(ids1);
+            //ids.Intersect(ids1);
         }
     }
 
