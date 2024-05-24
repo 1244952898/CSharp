68d3775b1ac60d30cea67cf58c2cb9847ccdc973
ZYU <1244952898@qq.com>
2024-05-24 14:28:01 +0800

    1

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CSharp
 {
@@ -8,114 +9,12 @@ namespace CSharp
     {
         static void Main(string[] args)
         {
-            var l=new TreeNode(1,null,null);
-            var r=new TreeNode(2,null,null);
-            var root=new TreeNode(3,l,r);
-
-            var lst = PreorderTraversal(root);
+            Task.Factory.StartNew(Go);
         }
-        public static IList<int> PreorderTraversal(TreeNode root)
+        static void Go()
         {
-            var nodeList = new List<TreeNode>();
-            nodeList.Add(root);
-            var results = new List<int>();
-            while (nodeList.Count > 0)
-            {
-                if (nodeList[0].left != null)
-                {
-                    nodeList.Add(nodeList[0].left);
-                }
-                if (nodeList[0].right != null)
-                {
-                    nodeList.Add(nodeList[0].right);
-                }
-                results.Add(nodeList[0].val);
-                nodeList.RemoveAt(0);
-            }
-            return results;
-        }
-
-        public int EvalRPN(string[] tokens)
-        {
-            var numberStack = new Stack<string>();
-            foreach (var s in tokens)
-            {
-                if (s == "+")
-                {
-                    var n0 = int.Parse(numberStack.Pop());
-                    var n1 = int.Parse(numberStack.Pop());
-                    numberStack.Push((n1 + n0).ToString());
-                }
-                else if (s == "-")
-                {
-                    var n0 = int.Parse(numberStack.Pop());
-                    var n1 = int.Parse(numberStack.Pop());
-                    numberStack.Push((n1 - n0).ToString());
-                }
-                else if (s == "*")
-                {
-                    var n0 = int.Parse(numberStack.Pop());
-                    var n1 = int.Parse(numberStack.Pop());
-                    numberStack.Push((n1 * n0).ToString());
-                }
-                else if (s == "/")
-                {
-                    var n0 = int.Parse(numberStack.Pop());
-                    var n1 = int.Parse(numberStack.Pop());
-                    numberStack.Push((n1 / n0).ToString());
-                }
-                else
-                {
-                    numberStack.Push(s);
-                }
-            }
-            return int.Parse(numberStack.Pop());
-        }
-
-        static IEnumerable<int> GetYeild()
-        {
-            Console.WriteLine("yeild 开始");
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine($"yeild 开始执行{i}");
-                yield return i;
-                Console.WriteLine($"yeild 执行结束{i}");
-            }
-            Console.WriteLine("yeild 停止当前");
-        }
-
-        public static int[] GetNext(string s)
-        {
-            var next = new int[s.Length];
-            var j = 0;
-            next[0] = 0;
-            for (int i = 1; i < s.Length; i++)
-            {
-                while (j > 0 && s[j] != s[i])
-                {
-                    j = next[j - 1];
-                }
-
-                if (s[j] == s[i])
-                {
-                    j++;
-                }
-                next[i] = j;
-            }
-            return next;
-        }
-    }
-
-    public class TreeNode
-    {
-        public int val;
-        public TreeNode left;
-        public TreeNode right;
-        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
-        {
-            this.val = val;
-            this.left = left;
-            this.right = right;
+            var isPool = Thread.CurrentThread.IsThreadPoolThread;
+            Console.WriteLine($"Hello from the thread pool!   {isPool}");
         }
     }
 }
--- CSharpCore/Program.cs
+++ CSharpCore/Program.cs
@@ -1,13 +1,48 @@
 ﻿using wwm.LeetCodeHelper;
-using Microsoft.Extensions.Configuration;
 
 namespace CSharpCore
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine(11111111);
+            var lst = new List<string>() { "1", "2", "3", "4", "5", "6" };
+            (lst[1], lst[4]) = (lst[3], lst[1]);
+            lst.RemoveAt(0);
+
+            ThreadPool.GetMaxThreads(out var maxWorkerThreads, out var maxConcurrentActiveRequests);
+            Func<string, int> method = Work;
+            IAsyncResult cookie = method.BeginInvoke("test", null, null);
+            //
+            // ... here's where we can do other work in parallel...
+            //
+            int result = method.EndInvoke(cookie);
+            Console.WriteLine("String length is: " + result);
+        }
+
+        static int Work(string s)
+        {
+            return s.Length;
+        }
+        public static IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            var results = new List<IList<int>>();
+            var nodeList = new List<TreeNode>
+            {
+                root
+            };
+            while (nodeList.Count > 0)
+            {
+                var cnt = nodeList.Count;
+                var rs = new List<int>();
+                for (var i = 0; i < cnt; i++)
+                {
+                    rs.Add(nodeList[0].val);
+                    nodeList.RemoveAt(0);
+                }
+                results.Add(rs);
+            }
+            return results;
         }
     }
 }
