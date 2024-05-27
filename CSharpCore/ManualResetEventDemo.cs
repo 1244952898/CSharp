dfc06e05a8ef8c355166bec9566080a3bd0a3c62
ZYU <1244952898@qq.com>
2024-05-27 15:24:34 +0800

    ManualResetEventDemo#Thread

---
+++ CSharpCore/ManualResetEventDemo.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpCore
+{
+    public class ManualResetEventDemo
+    {
+        static ManualResetEvent _starter = new(false);
+        static void Main1()
+        {
+            RegisteredWaitHandle reg = ThreadPool.RegisterWaitForSingleObject(_starter, Go, "Some Data", -1, true);
+            Thread.Sleep(1000);
+            Console.WriteLine("Signaling worker...");
+            _starter.Set();
+            int i = 10;
+            reg.Unregister(_starter);    // Clean up when we’re done.
+        }
+
+        public static void Go(object data, bool timedOut)
+        {
+            Console.WriteLine("Started - " + data);
+        }
+
+    }
+}
