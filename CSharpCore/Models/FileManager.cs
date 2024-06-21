a60844518349fd76e25f8fcd7369a65cc226d1bb
ZYU <1244952898@qq.com>
2024-06-21 16:21:31 +0800

    1

---
+++ CSharpCore/Models/FileManager.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace CSharpCore.Models
+{
+    public class FileManager : IFileManager
+    {
+        private readonly IFileProvider _fileProvider;
+
+        public FileManager(IFileProvider fileProvider)
+        {
+            _fileProvider = fileProvider;
+        }
+
+        public async Task<string> ReadAllTextAsync(string path)
+        {
+            using var stream = _fileProvider.GetFileInfo(path).CreateReadStream();
+            var buffers=new byte[stream.Length];
+            await stream.ReadAsync(buffers,0, buffers.Length);
+            return Encoding.Default.GetString(buffers);
+        }
+
+        public void ShowStructor(Action<int, string> render)
+        {
+            Render(string.Empty, -1);
+            void Render(string subPath, int index)
+            {
+                index++;
+                var files = _fileProvider.GetDirectoryContents(subPath);
+                foreach (var fileinfo in files)
+                {
+                    render(index, fileinfo.Name);
+                    if (fileinfo.IsDirectory)
+                    {
+                        Render($@"{subPath}\{fileinfo.Name}".TrimStart('\\'), index);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
--- CSharpCore/Program.cs
+++ CSharpCore/Program.cs
@@ -1,6 +1,8 @@
 ﻿using CSharpCore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using System.Reflection;
-using System.Reflection.Emit;
 
 namespace CSharpCore
 {
@@ -11,15 +13,33 @@ namespace CSharpCore
         static void Main()
         {
             //RestProxyCreator.BuildAssembly();
-            var dllName = "MyDynamic";
-            var title = "This is a dynamic title.";
-            AssemblyName assemblyName = new AssemblyName(dllName)
-            {
-                Version = new Version("3.0.0.0")
-            };
-            var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
-            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("DefineDynamicModule");
-            moduleBuilder.DefineEnum("DefineEnum", TypeAttributes.Public, typeof(int));
+            //var dllName = "MyDynamic";
+            //var title = "This is a dynamic title.";
+            //AssemblyName assemblyName = new AssemblyName(dllName)
+            //{
+            //    Version = new Version("3.0.0.0")
+            //};
+            //var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+            //ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("DefineDynamicModule");
+            //moduleBuilder.DefineEnum("DefineEnum", TypeAttributes.Public, typeof(int));
+
+            //static void Print(int layer, string name) => Console.WriteLine($"{new string(' ', layer * 4)}{name}");
+            //var fileManager = new ServiceCollection()
+            //    .AddSingleton<IFileProvider>(new PhysicalFileProvider(@"c:\test"))
+            //    .AddSingleton<IFileManager, FileManager>()
+            //    .BuildServiceProvider()
+            //    .GetService<IFileManager>();
+
+            //fileManager.ShowStructor(Print);
+            //var txt = fileManager.ReadAllTextAsync("data.txt").GetAwaiter().GetResult();
+
+            var assemble = Assembly.GetEntryAssembly();
+            var fileManager1 = new ServiceCollection()
+                .AddSingleton<IFileProvider>(new EmbeddedFileProvider(assemble))
+                .AddSingleton<IFileManager, FileManager>()
+                .BuildServiceProvider()
+                .GetService<IFileManager>();
+            var txt = fileManager1.ReadAllTextAsync("data.txt").GetAwaiter().GetResult();
         }
 
     }
