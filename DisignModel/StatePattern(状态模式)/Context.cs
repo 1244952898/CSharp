c03e76995addbc1a35e3a2297546780550f411e6
ZYU <1244952898@qq.com>
2024-09-09 09:23:43 +0800

    1

---
+++ DisignModel/StatePattern(状态模式)/Context.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisignModel.StatePattern_状态模式_
+{
+    public class Context
+    {
+        public int Count {  get; set; }
+
+    }
+}
